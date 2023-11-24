21ef31fbc52ca748c3383ca82ed2ea33b793cb1c
mail@zaufall.de <mail@zaufall.de>
2023-11-24 17:30:14 +0100

    tidy up

--- python/config.py
+++ python/config.py
@@ -15,14 +15,18 @@ LEVELS_DEEP = int('5')
 if CLOUD_TYPE == "OPENAI":
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY_NATIVE')
     OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
+    OPENAI_DEPLOYMENT = ""
+    OPENAI_API_VERSION = ""
+
     API_URL = OPENAI_API_URL
     KEY_HEADER_TEXT = "Authorization"
     KEY_HEADER_VALUE = "Bearer " & OPENAI_API_KEY
 else:
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    OPENAI_API_BASE = os.getenv('OPENAI_API_BASE')
+    OPENAI_API_URL = os.getenv('OPENAI_API_BASE')
     OPENAI_DEPLOYMENT = os.getenv('OPENAI_DEPLOYMENT')
     OPENAI_API_VERSION = os.getenv('OPENAI_API_VERSION')
-    API_URL = OPENAI_API_BASE + "openai/deployments/" + OPENAI_DEPLOYMENT + "/chat/completions?api-version=" + OPENAI_API_VERSION
+
+    API_URL = OPENAI_API_URL + "openai/deployments/" + OPENAI_DEPLOYMENT + "/chat/completions?api-version=" + OPENAI_API_VERSION
     KEY_HEADER_TEXT = "api-key"
     KEY_HEADER_VALUE = OPENAI_API_KEY
