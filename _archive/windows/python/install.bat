02a212b49a399244a520d4261c40e0e86bf830fb
mail@zaufall.de <mail@zaufall.de>
2023-12-05 16:47:07 +0100

    remove old files

--- _archive/windows/python/install.bat
+++
@@ -1,7 +0,0 @@
-pip install --upgrade openai
-pip install --upgrade requests
-
-rem pip install --upgrade pypiwin32 
-rem https://github.com/mhammond/pywin32/releases
-pip install --upgrade pywin32
-pause
